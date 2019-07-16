081e7fb2eb7903da9f3d8bc30df574f189827473
Orpheas <orpheas@boomfantasy.com>
2019-07-16 19:52:04 -0400

    refactored to not assume lineups are correct

--- nbaHackathon.py
+++ nbaHackathon.py
@@ -16,19 +16,21 @@ for name, group in groupedPlays:
 	sortedPlays = group.sort_values(["Period", "PC_Time", "WC_Time", "Event_Num"], ascending=[True, False, True, True])
 	lineup = groupedLineups.get_group(name)
 	byPeriod = lineup.groupby("Period")
-	startingLineup = byPeriod.get_group(1)
-	onCourt = defaultdict(list)
+	# startingLineup = byPeriod.get_group(1)
+	# onCourt = defaultdict(list)
 	playerStats = {}
+	onCourt = []
 	#print startingLineup
-	for index, row in startingLineup.iterrows():
-		teamId = row["Team_id"]
-		playerId = row["Person_id"]
-		onCourt.setdefault(teamId, []).append(playerId)
+	# for index, row in startingLineup.iterrows():
+	# 	teamId = row["Team_id"]
+	# 	playerId = row["Person_id"]
+	# 	onCourt.setdefault(teamId, []).append(playerId)
 		# As substitutions happen, need to update onCourt for each team
 
 	gameRoster = byPeriod.get_group(0)
 	activePlayers = gameRoster[gameRoster['status'] == 'A']
-
+	teamIds = gameRoster['Team_id'].unique()
+	print teamIds
 	for index, row in activePlayers.iterrows():
 		teamId = row["Team_id"]
 		playerId = row["Person_id"]
@@ -38,6 +40,7 @@ for name, group in groupedPlays:
 			"offense": 0.0,
 			"defense": 0.0,
 		}
+		
 	for index, row in sortedPlays.iterrows():
 		eventType = row["Event_Msg_Type"]
 		eventNum = row["Event_Num"]
@@ -47,39 +50,34 @@ for name, group in groupedPlays:
 		period = row["Period"]
 		
 		if eventType == EventTypes.startPeriod:
-			if period > 1:
-				onCourt = defaultdict(list)
-				periodLineup = byPeriod.get_group( period )
-				for index, row in periodLineup.iterrows():
-					teamId = row["Team_id"]
-					playerId = row["Person_id"]
-					onCourt.setdefault(teamId, []).append(playerId)
+			onCourt = []
+			possesions = 0.0
+			score = {
+				teamIds[0]: 0.0,
+				teamIds[1]: 0.0,
+			}
+
+		# Loop through each play, if a basket occured, increment the appropriate score,
+		# and increment offense or defensive stats for all players in onCourt
+		# At the end of each possession, increment # of possessions
+		# and increment possessions for all players in onCourt
+		# period timers will be added to players who were subbed out because
 
 		if eventType == EventTypes.substitution:
-			
-			for lineup in onCourt.values():
-				if player1 not in lineup:
-					print "player not in this lineup"
-				else:
-					print lineup
-					lineup.remove(player1)
-					lineup.append(player2)
-					print "made sub"
-					print lineup
-			# if player1 not in onCourt[teamId]:
-			# 	print "player not in game"
+			if player1 not in onCourt:
+				# not in our lineup so we give them the stats from the start of the period
+				stats = playerStats[ player1 ]
+				playerTeam = stats["teamId"]
+				oppositeTeam = next( item for item in teamIds if item != playerTeam )
+				stats["offense"] += score[ playerTeam ]
+				stats["defense"] += score[ oppositeTeam ]
+				stats["possesions"] += possesions
 				
-			# else:
-			# 	#print onCourt[teamId]
-			# 	#print player1, player2
-			# 	onCourt[teamId].remove( player1 )
-			# 	onCourt[teamId].append( player2 )
-			# 	print "made sub"
-			
+			else:
+				# the players will already be accounted for because theyre on court
+				onCourt.remove( player1 )
+				onCourt.append( player2 )
 
-	### Todo at the start of a new period set the lineup again? 
-
-	#print playerStats
 	if name == "006728e4c10e957011e1f24878e6054a":
 		break;
 	
